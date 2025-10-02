32230eabd3fc8d14f0954c3d829136e44f602f51
denyocr <denyocr.world@gmail.com>
2025-10-02 12:09:25 +0700

    setup GitHub Actions workflow for Flutter

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 android {
     namespace = "com.example.test_ci_cd"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -27,7 +27,7 @@ android {
         applicationId = "com.example.test_ci_cd"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 29
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
